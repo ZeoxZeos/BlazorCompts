6059280820df87e09ac7201235ddf8d47fc84946
Zenon Iwanicki <zenon@cleveradviser.com>
2019-02-08 16:34:24 +0000

    Add project files.

---
+++ BlazorCompts/Pages/BlazorRocksBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Blazor.Rendering;
+namespace BlazorCompts.Pages
+{
+    public class BlazorRocksBase : ComponentBase
+    {
+
+        public string BlazorRocksText { get; set; } = "Blazor rocks the browser!";
+    }
+}
